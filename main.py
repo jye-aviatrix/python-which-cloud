420e936d43251f8f07cae944cec23e5b85a6e440
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2023-06-20 13:46:20 -0400

    Add ali-cloud

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ logging.basicConfig(
 )
 
 # Use standard urllib request
-
-
-def send_get_request_with_headers(url, headers):
+timeout=5 # Default timeout 5 seconds
+def send_get_request_with_headers(url, headers, timeout=timeout):
     try:
         req = urllib.request.Request(url, headers=headers)
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(req, timeout=timeout) as response:
             data = response.read().decode("utf-8")
             return data
     except urllib.error.HTTPError as e:
@@ -28,7 +27,7 @@ def send_get_request_with_headers(url, headers):
         # print(f"URL Error: {e.reason}")
         logging.error("HTTP Error: %s", e.reason)
 
-
+# Check if string is valid json
 def is_valid_json(data):
     try:
         if data is not None and data.strip():  # Check if the string is not empty or null
@@ -38,9 +37,7 @@ def is_valid_json(data):
         pass
     return False
 
-# Check Azure meta data service
-
-
+# Check against Azure metadata service
 def is_azure():
     try:
         url = "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
@@ -62,7 +59,7 @@ def is_azure():
         logging.error("Exception occurred: %s", str(e))
         return False
 
-
+# Check against AWS metadata service
 def is_aws():
     try:
         url = "http://169.254.169.254/latest/meta-data/ami-id"
@@ -79,7 +76,7 @@ def is_aws():
         logging.error("Exception occurred: %s", str(e))
         return False
 
-
+# Check against GCP metadata service
 def is_gcp():
     try:
         url = "http://metadata.google.internal/computeMetadata/v1/instance/zone"
@@ -102,9 +99,22 @@ def is_oci():
     pass
 
 
-def is_alicloud():
-    # Perform AliCloud check here
-    pass
+# Check against Ali Cloud metadata service
+def is_aliyun():
+    try:
+        url = "http://100.100.100.200/latest/meta-data/region-id"
+        custom_headers = {}
+        logging.info("Try to check if VM is running in AliCloud/AliYun")
+        logging.info("Query %s: ", url)
+        response_data = send_get_request_with_headers(
+            url, headers=custom_headers)
+        if isinstance(response_data, str):
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.error("Exception occurred: %s", str(e))
+        return False
 
 
 def check_cloud_provider():
@@ -117,15 +127,22 @@ def check_cloud_provider():
         return "gcp"
     elif is_oci():
         return "oci"
-    elif is_alicloud():
-        return "alicloud"
+    elif is_aliyun():
+        return "aliyun"
     else:
         return None  # Return None or handle the case when no provider is detected
 
 
-# Usage example
-cloud_provider = check_cloud_provider()
-if cloud_provider:
-    print(f"The detected cloud provider is: {cloud_provider}")
-else:
-    print("No cloud provider detected.")
+# return finding
+def main():
+    cloud_provider = check_cloud_provider()
+    if cloud_provider:
+        logging.info("The detected cloud provider is: %s", cloud_provider)
+    else:
+        logging.info("No cloud provider detected.")
+    return cloud_provider
+
+
+if __name__ == "__main__":
+    value = main()
+    print(value)
